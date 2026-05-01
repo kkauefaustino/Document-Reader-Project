7c067884120da6cae6b249d296caa8bb031b6c50
Kauê <kauefmfreitas@gmail.com>
2026-05-01 18:19:21 -0300

    Add pastas back e front

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+#Carrega as variáveis do arquivo .env para o sistema
+load_dotenv()
+
+#Agora conseguimos acessar a chave de forma segura
+api_key = os.getenv("GEMINI_API_KEY")
+
+print("Chave carregada com sucesso")
