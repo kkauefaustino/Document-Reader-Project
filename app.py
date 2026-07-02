f57dc45aa912986cbc70a5e9d5552e0b1bc1b691
Kauê <kauefmfreitas@gmail.com>
2026-07-02 15:46:08 -0300

    Integração completa com a API do Gemini 2.5-flash e chat funcional

--- app.py
+++ app.py
@@ -46,6 +46,51 @@ if st.session_state.tela_atual == "tela_upload":
             st.rerun()
     
 elif st.session_state.tela_atual == "tela_chat":
-    st.title("Conteúdo do PDF")
+    from google import genai
 
-    st.text_area("Texto extraído: ", value=st.session_state.pdf_txt, height=400)
+    st.title("Converse com o seu PDF")
+
+    if "historico" not in st.session_state:
+        st.session_state.historico = [{"role": "assistant", "content": "Olá! Já li o seu PDF. O que gostaria de perguntar?"}]
+    
+    if st.button("Enviar outro arquivo"):
+        st.session_state.tela_atual = "tela_upload"
+        st.session_state.pdf_txt = ""
+        del st.session_state.historico
+        st.rerun()
+    
+    st.divider()
+
+    for mensagem in st.session_state.historico:
+        with st.chat_message(mensagem["role"]):
+            st.write(mensagem["content"])
+
+
+    if pergunta_usuario := st.chat_input("Pergunte algo sobre o documento"):
+
+        with st.chat_message("user"):
+            st.write(pergunta_usuario)
+        st.session_state.historico.append({"role": "user", "content": pergunta_usuario})
+
+        client = genai.Client(api_key=api_key)
+
+        prompt = f"""
+        Você é um assistente especializado em análise de documentos.
+        Use o seguinte texto extraído de um PDF para responder à pergunta do usuário.
+        
+        Texto do PDF:
+        {st.session_state.pdf_txt}
+        
+        Pergunta:
+        {pergunta_usuario}
+        """
+
+        resposta_ia = client.models.generate_content(
+            model= "gemini-2.5-flash",
+            contents= prompt
+        )
+
+        with st.chat_message("assistant"):
+            st.write(resposta_ia.text)
+        st.session_state.historico.append({"role": "assistant", "content": resposta_ia.text})
+
